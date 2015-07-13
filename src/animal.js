a026e094af836af9056fe505d454f3d89e5bf47b
Zack Claar <claar.zack@gmail.com>
2015-07-13 10:47:12 -0600

    changed to tests brnach

--- src/animal.js
+++ src/animal.js
@@ -1,9 +1,43 @@
-function Animal(){
-
+function Animal(name, age, kind){
+  this.name = name;
+  this.age = age;
+  this.kind = kind;
 }
 
 Animal.prototype.oink = function(){
+  if(this.kind === "Pig") {
+    return "oink";
+  } else {
+    return "I am not a pig";
+  }
+};
 
+Animal.prototype.growUp = function() {
+  // this.age += 1;
+  return this.age += 1;
+};
+
+Animal.prototype.awake = function() {
+  this.awake = "off";
+  return this.awake;
+};
+
+Animal.prototype.wakeUp = function() {
+  this.awake = "awake";
+  return this.awake;
+};
+
+Animal.prototype.sleep = function() {
+  this.sleeping = "sleeping";
+  return this.sleeping;
+};
+
+Animal.prototype.feed = function() {
+  if(this.awake === "awake") {
+    this.feed = "fed";
+  } else {
+    this.feed = "not fed";
+  }
 };
 
 module.exports=Animal;
